aaa9a489a05db549523527208ebdab3bddaf1a6a
Desai <rahul-mangesh.desai@capgemini.com>
2021-12-14 20:12:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import './App.css';
+const App = () => {
+  ////////////////console.log('state'+ state)
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Rahul");
+
+//events
+const IncNum = () => {
+    setCount(count+1);
+}
+
+const DecNum = () => {
+  setCount(count-1);
+}
+
+const NameOnClick = () => {
+  
+  switch(name){
+    case "Rahul":
+    setName("Desai");
+    break;
+    case  "Desai":
+      setName("Rahul")
+  }
+
+
+}
+  return (
+   <div className="App">
+    <button onClick={IncNum}>+</button>
+     <h1>{count}</h1>
+    <button onClick={DecNum}>-</button>
+    <h1>{name}</h1>
+    <button onClick={NameOnClick}>Click me!</button>
+   </div>
+  );
+}
+ 
+export default App;
